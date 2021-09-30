5352dfce84b983b5f2eba41378307487985a9d9c
MarkBoyle1 <mark.boyle@myob.com>
2021-09-30 17:22:23 +1000

    Add numbers less than maxDigits to a prefix list.

--- PhoneNumbers.Tests/PhoneNumbersTests.cs
+++ PhoneNumbers.Tests/PhoneNumbersTests.cs
@@ -8,13 +8,28 @@ namespace PhoneNumbers.Tests
     public class PhoneNumbersTests
     {
         [Fact]
-        public void Test1()
+        public void given_phoneNumbersContains_91125426_97625992_911_when_FindMaxNumberOfDigits_then_return_8()
         {
             List<string> phoneNumbers = new List<string>() {"91125426", "97625992", "911"};
+            Stack<string> myStack = new Stack<string>(phoneNumbers);
 
-            int maxDigits =  PhoneBookAnalyser.FindMaxNumberOfDigits(0, phoneNumbers, 0);
+            int maxDigits =  PhoneBookAnalyser.FindMaxNumberOfDigitsTailRecursion(myStack);
             
             Assert.Equal(8, maxDigits);
+        }
+        
+        [Fact]
+        public void given_phoneNumbersContains_91125426_97625992_911_when_AddingNumbersToPrefixList_then_return_911()
+        {
+            List<string> phoneNumbers = new List<string>() {"91125426", "97625992", "911"};
+            Stack<string> myStack = new Stack<string>(phoneNumbers);
+            List<string> prefixs = new List<string>();
+
+            List<string> expected = new List<string>() {"911"};
+
+            List<string> actual =  PhoneBookAnalyser.AddingNumbersToPrefixList(prefixs, myStack, 8);
+            
+            Assert.Equal(expected, actual);
         }
     }
 }
--- PhoneNumbers/Program.cs
+++ PhoneNumbers/Program.cs
@@ -1,12 +1,85 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PhoneNumbers
 {
-    class Program
+    public class PhoneBookAnalyser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> phoneNumbers = new List<string>() {"91125426", "97625992", "911"};
+            Stack<string> myStack = new Stack<string>(phoneNumbers);
+            
+
+            List<string> prefixs = new List<string>();
+
+            FindMaxNumberOfDigits(0, phoneNumbers, 0);
         }
+
+        public static int FindMaxNumberOfDigits(int i, List<string> phoneNumbers, int maxNumberOfDigits)
+        {
+            int numberOfDigits = phoneNumbers[i].Length;
+            
+            if (numberOfDigits > maxNumberOfDigits)
+            {
+                maxNumberOfDigits = numberOfDigits;
+            }
+            
+            if (i < phoneNumbers.Count - 1)
+            {
+                FindMaxNumberOfDigits(i + 1, phoneNumbers, maxNumberOfDigits);
+            }
+            
+            return maxNumberOfDigits;
+        }
+        
+        public static List<string> AddingNumbersToPrefixList(List<string> prefixs, Stack<string> myStack, int maxNumberOfDigits)
+        {
+            string phoneNumber = myStack.Pop();
+            
+            if (myStack.Count == 0)
+            {
+                if (maxNumberOfDigits != phoneNumber.Length)
+                {
+                    prefixs.Add(phoneNumber);
+                }
+
+                return prefixs;
+            }
+
+            prefixs = AddingNumbersToPrefixList(prefixs, myStack, maxNumberOfDigits);
+
+            if (maxNumberOfDigits != phoneNumber.Length)
+            {
+                prefixs.Add(phoneNumber);
+            }
+
+            return prefixs;
+        }
+        
+        
+        
+        
+        
+        
+        public static int FindMaxNumberOfDigitsTailRecursion(Stack<string> myStack)
+        {
+            int numberOfDigits = myStack.Pop().Length;
+
+            if (myStack.Count == 1)
+            {
+                return numberOfDigits;
+            }
+
+            int recursionFunction = FindMaxNumberOfDigitsTailRecursion(myStack);
+        
+            if (numberOfDigits >= recursionFunction)
+            {
+                return numberOfDigits;
+            }
+        
+            return recursionFunction;
+        }
+
     }
 }
